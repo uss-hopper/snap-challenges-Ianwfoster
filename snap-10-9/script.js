21efce619f2d934139677f85a86ab749bc59ebbd
Ian Foster <ifoster2@cnm.edu>
2019-10-09 11:20:51 -0600

    commit 2

---
+++ snap-10-9/script.js
@@ -0,0 +1,12 @@
+function calculatePeperoniNeeded(pizza, peperoniPerPizza) {
+	return pizza * peperoniPerPizza;
+}
+
+console.log(calculatePeperoniNeeded(pizza, peperoniPerPizza))
+
+
+function concatStrings(firstName, lastName) {
+	return firstName +''+ lastName
+
+}
+console.log(concatStrings())
