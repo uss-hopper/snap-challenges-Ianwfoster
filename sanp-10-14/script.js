7bbce259b0de78c38a943bc9aa468458be06aacd
Ian Foster <ifoster2@cnm.edu>
2019-10-14 09:40:37 -0600

    snap 14-part 1

--- sanp-10-14/script.js
+++ sanp-10-14/script.js
@@ -1,4 +1,21 @@
-/* When the input field receives input, convert the value from pounds to kilograms */
-function weightCoverter(valNum) {
-	document.getElementById("outputGrams") .innerHTML = valNum / 0.0022046
+/**
+ * Convert a weight or mass to grams.
+ *
+ * @param weight The weight or mass to be converted.
+ * @param units The abbreviation for the units in which this weight/mass is measured. Acceptable values: "lbs", "oz", "g", "kg', "mg".
+ * @returns The mass in grams.
+ */
+function weightConverter(weight, units) {
+
+	if(units.toLowerCase() === "lbs"){
+		weight = weight * 453.592;
+	} else if (units.toLowerCase() === "oz") {
+		weight = weight * 28.3495;
+	}else if (units.toLowerCase() === "kg") {
+		weight = weight * 1000;
+	}else if (units.toLowerCase() === "mg") {
+		weight = weight / 1000;
+	}
+
+	return weight;
 }
