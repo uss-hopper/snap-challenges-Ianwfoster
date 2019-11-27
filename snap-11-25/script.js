dcfc2cfcc45bca13018eeab1fa2df6cf6ae01057
Ian Foster <ifoster2@cnm.edu>
2019-11-27 08:44:58 -0700

    new snap challenge attempt

--- snap-11-25/script.js
+++ snap-11-25/script.js
@@ -1,13 +1,34 @@
-const vowels = "a,e,i,o,u,"
 
-function  removeVowels() {
-	if vowels in words  (" ");
-	result = delete "a,e,i,o,u"
+//function  removeVowels() {
+//let vowels = "a,e,i,o,u,"
+//	if vowels in words  (" ");
+//	result = delete "a,e,i,o,u"
+//}
+/**
+ *
+ **/
+
+ function removeVowels(string toRemove){
+	let vowels = ["a,e,i,o,u,"];
+	let toReturn =" "
+	foreach (character in toRemove){
+		if(vowels  dosen't contains character){
+			toReturn =toReturn . character;
+		}
+
+	}
 }
 
 
 const numbers = "1, 2, 3, 4, 5"
-function removeNumbers() {
+function findGreatestNumber(array myNumber) {
+	let greatest = myNumber [0];
+	foreach(myNumber as number){
+		if(greatest<number){
+			greatest=> number;
+			return greatest
+		}
+	}
 	if (number) is < 5; {
 		result delete number;
 	}
