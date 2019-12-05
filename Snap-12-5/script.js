8ad69e7829e7aaf21baeca78452ba41b76f22c79
Ian Foster <ifoster2@cnm.edu>
2019-12-05 09:50:25 -0700

    leaning to use axios

--- Snap-12-5/script.js
+++ Snap-12-5/script.js
@@ -7,20 +7,39 @@
 // 	let target = await axios(configAPI)
 // 	console.log(target.status)
 // }
+//
+
+// const axios = require('axios');
+//
+// async function makeRequest() {
+//
+// 	const config = {
+// 		method: 'get',
+// 		url: "https://images.dog.ceo/breeds/germanshepherd/n02106662_7168.jpg"
+// 	}
+//
+// 	let target = await axios(config)
+//
+//
+// }
+//
+// makeRequest(target.status);
 
 
-const axios = require('axios');
 
-async function makeRequest() {
-
-	const config = {
-		method: 'get',
-		url: "https://images.dog.ceo/breeds/germanshepherd/n02106662_7168.jpg"
-	}
-
-	let target = await axios(config)
-
-	console.log(target.status);
+const onPageLoad = ()=> {
+	axios.get('https://dog.ceo/api/breeds/list/all').then(({data}) =>{
+		let breeds= data.message;
+		let dogs = ' ';
+		for(let breed in breeds) {
+			dogs = dogs + '<div>' + breed + '</div>';
+		}
+		let html = document.getElementById("target");
+		html.innerHTML = dogs;
+		});
 }
 
-makeRequest();
+
+
+
+
