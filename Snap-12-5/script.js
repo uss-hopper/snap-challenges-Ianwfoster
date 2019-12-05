249061d26337a7db140897802ffc1811ebe3c4fd
Ian Foster <ifoster2@cnm.edu>
2019-12-05 09:10:13 -0700

    leaning to use axios

--- Snap-12-5/script.js
+++ Snap-12-5/script.js
@@ -1,9 +1,26 @@
+// const axios = require('axios');
+//
+// async function callDogApi() {
+// 	const configAPI = {
+// 		getElementById : "h2"
+// 	}
+// 	let target = await axios(configAPI)
+// 	console.log(target.status)
+// }
+
+
 const axios = require('axios');
 
-async function callDogApi() {
-	const configAPI = {
+async function makeRequest() {
+
+	const config = {
 		method: 'get',
-		getElementById : "h2"
+		url: "https://images.dog.ceo/breeds/germanshepherd/n02106662_7168.jpg"
 	}
-	let target = await axios(configAPI)
+
+	let target = await axios(config)
+
+	console.log(target.status);
 }
+
+makeRequest();
